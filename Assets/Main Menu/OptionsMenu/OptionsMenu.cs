51b8121e32a7226920dd5405119a8d8963778904
cody.Kall <supeamoverlord@gmail.com>
2022-04-29 22:27:57 -0500

    options menu graphics resolution update

--- Assets/Main Menu/OptionsMenu/OptionsMenu.cs
+++ Assets/Main Menu/OptionsMenu/OptionsMenu.cs
@@ -21,14 +21,26 @@ public class OptionsMenu : MonoBehaviour
 
           List<string> options = new List<string>();
 
+          int currentResolutionIndex = 0;
+
           for(int i = 0; i < resolutions.Length; i++)
           {
                string option = resolutions[i].width + " x " + resolutions[i].height;
 
                options.Add(option);
+
+               if(resolutions[i].width == Screen.currentResolution.width &&
+                    resolutions[i].height == Screen.currentResolution.height)
+               {
+                    currentResolutionIndex = i;
+               }
           }
 
           resolutionDropdown.AddOptions(options);
+
+          resolutionDropdown.value = currentResolutionIndex;
+          resolutionDropdown.RefreshShownValue();
+
      }
 
      public void SetVolume (float volume)
@@ -45,5 +57,12 @@ public class OptionsMenu : MonoBehaviour
      public void SetFullscreen(bool isFullscreen)
      {
           Screen.fullScreen = isFullscreen;
+     }
+
+
+     public void SetResolution(int resolutionIndex)
+     {
+          Resolution resolution = resolutions[resolutionIndex];
+          Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
      }
 }
