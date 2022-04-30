4ab10d88c1173575fac6daae83b8c2ef598dfc1c
cody.Kall <supeamoverlord@gmail.com>
2022-04-29 19:13:45 -0500

    added main menu and UI

---
+++ Assets/Main Menu/OptionsMenu/OptionsMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+   
+     public AudioMixer audioMixer;
+
+     Resolution[] resolutions;
+
+     public Dropdown resolutionDropdown;
+
+     private void Start()
+     {
+          resolutions = Screen.resolutions;
+
+          resolutionDropdown.ClearOptions();
+
+          List<string> options = new List<string>();
+
+          for(int i = 0; i < resolutions.Length; i++)
+          {
+               string option = resolutions[i].width + " x " + resolutions[i].height;
+
+               options.Add(option);
+          }
+
+          resolutionDropdown.AddOptions(options);
+     }
+
+     public void SetVolume (float volume)
+     {
+          Debug.Log(volume);
+          audioMixer.SetFloat("volume", volume);
+     }
+
+     public void SetQuality(int qualityIndex)
+     {
+          QualitySettings.SetQualityLevel(qualityIndex);
+     }
+
+     public void SetFullscreen(bool isFullscreen)
+     {
+          Screen.fullScreen = isFullscreen;
+     }
+}
