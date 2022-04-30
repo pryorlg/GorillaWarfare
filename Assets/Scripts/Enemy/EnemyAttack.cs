5b135d9384b6a07a847eab70c6345a53ea996dae
pryorg <pryorg08@gmail.com>
2022-04-29 23:12:21 -0500

    Added EnemyAttack script, animations, and sprites. Still working on implementation

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator anim; // Varaible to store the Animator component information.
+    private EnemyMovement enemyMovement; // Variable that allows access to the EnemyMovement script functions.
+    private BoxCollider2D boxCollider; // Variable to store the boxCollider component information.
+
+    [SerializeField] private LayerMask playerLayer; // Variable to store the playerLayer layer information.
+
+    private void Awake()
     {
-        
+        anim = GetComponent<Animator>(); // Stores the Animator component from the game object.
+        enemyMovement = GetComponent<EnemyMovement>(); // Stores the EnemyMovement components from the EnemyMovement script
+        boxCollider = GetComponent<BoxCollider2D>(); // Stores the BoxCollider2D component from the game object.
     }
 
-    // Update is called once per frame
     void Update()
     {
+
+
         
+        Debug.DrawRay(new Vector2(boxCollider.bounds.center.x + (boxCollider.bounds.extents.x * -transform.localScale.x), boxCollider.bounds.center.y), (Vector2.right*-transform.localScale.x) * 0.5f, Color.red);
+    }
+
+    private bool inRange()
+    {
+        RaycastHit2D raycastHitPlayer = Physics2D.Raycast(
+            new Vector2(boxCollider.bounds.center.x + (boxCollider.bounds.extents.x * -transform.localScale.x), 
+                        boxCollider.bounds.center.y), 
+                        (Vector2.right*-transform.localScale.x), 
+                        0.5f, 
+                        playerLayer);
+
+        return raycastHitPlayer.collider != null;
     }
 }
